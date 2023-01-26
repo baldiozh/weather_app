fd60a8a4aad8c0a0f30a30e4a52aa27ed7cb4898
Oksana <oksana@MacBook-Air-1.local>
2023-01-26 16:28:52 +0300

    fetch to API, getting locations, output ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
-const App = () => {
+import { useState, ChangeEvent } from "react"
+
+const App = (): JSX.Element => {
+	// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+	const [term, setTerm] = useState<string>('')
+	const [options, setOptions] = useState<[]>([])
+
+	const getSearchOptions = (value: string) =>{
+		fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()},
+			&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
+		.then((res) => res.json())
+		.then((data) => setOptions(data))
+	
+	}
+	const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value.trim()
+		setTerm(e.target.value)
+		// console.log(term)
+		if (value === '') return 
+		getSearchOptions(value)
+	}
+
 	return (
 	<main className="flex justify-center items-center bg-gradient-to-br
 		 from-sky-300 via-rose-300 to-lime-300 h-[100vh] w-full">
@@ -8,19 +29,33 @@ const App = () => {
 		  h-full lg:h-[500px] bg-white bg-opacity-20 
 		  backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
 		  <h1 >
-			  <span className="font-semi">Прогноз погоды</span>
+				<span className="font-semi">Прогноз погоды</span>
 		  </h1>
 		  <p className="text-sm font-light mt-2 py-2">
-			  Выбирайте место, погода которого вас интересует :)
+				Выбирайте место, погода которого вас интересует :)
 		  </p>
-		  <div className="flex mt-8 md:mt-4">
-			  <input type="text" value={''} 
-				  className="px-2 py-1 rounded-1-md border-2 border-white bg-white bg-opacity-75 ">
-			  </input>
+		  <div className="relative flex mt-8 md:mt-4">
+			  <input type="text" value={term} 
+					className="px-2 py-1 rounded-1-md border-2 border-white bg-white bg-opacity-75"
+					onChange={onInputChange}/>
+
+				<ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+					{options.map((option: {name: string}, index: number) => (
+						<li key={option.name + '-' + index}>
+							<button className="text-left text-sm w-full 
+							hover:bg-zinc-700 hover:text-white px-2 py-1
+							cursor-pointer">
+								{option.name}
+							</button>
+						</li> 
+					
+					))}
+				</ul>
+				
 			  <button className="rounded-r-md border-2 border-zinc-100 
-				  hover:border-zinc-500 hover:text-zinc-500 text-zinc-100
-				  px-2 py-1 cursor-pointer">
-				  Поиск
+					hover:border-zinc-500 hover:text-zinc-500 text-zinc-100
+					px-2 py-1 cursor-pointer">
+					Поиск
 			  </button>
 		  </div>
 		  
